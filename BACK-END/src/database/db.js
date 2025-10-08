230b93146ac5ed4ff4832dff64dbb6d37b1bd715
KYMA07 <ayacochokarina@gmail.com>
2025-10-08 08:45:31 +0800

    Delete db.js

--- BACK-END/src/database/db.js
+++
@@ -1,8 +0,0 @@
-// this is like fake for now cuz wla pa tayon gtotoong database
-
-export const db = {
-  query: (sql, params, callback) => {
-    console.log("🧠 Mock query executed:", sql, params);
-    callback(null, [{ message: "Mock data response" }]);
-  }
-};
