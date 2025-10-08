c1291100a0aba3a9a433eba5c45ca6fe902ab996
KYMA07 <ayacochokarina@gmail.com>
2025-10-08 11:14:56 +0800

    Update index.js

--- BACK-END/index.js
+++ BACK-END/index.js
@@ -1,25 +1,27 @@
-// index.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import sequelize from "./src/database/database.js";
 
-// Import routes
 import bookRoutes from "./routes/bookRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import borrowRoutes from "./routes/borrowRoutes.js";
 
 dotenv.config();
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/books", bookRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/borrow", borrowRoutes);
+
+app.use("/books", bookRoutes);
+app.use("/user", userRoutes);
+app.use("/borrow", borrowRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+sequelize.sync()
+  .then(() => console.log('Database connected and synced'))
+  .catch(err => console.error('Database sync error:', err));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
