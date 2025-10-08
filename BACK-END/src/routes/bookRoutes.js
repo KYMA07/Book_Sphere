c960083b0f0f4c9cc0b2c1d2f57d00f8856e95d7
KYMA07 <ayacochokarina@gmail.com>
2025-10-08 11:01:00 +0800

    Routes for the controllers
    coded

--- BACK-END/src/routes/bookRoutes.js
+++ BACK-END/src/routes/bookRoutes.js
@@ -4,9 +4,9 @@ import { getAllBooks, addBook, updateBook, deleteBook } from "../controllers/boo
 
 const router = express.Router();
 
-router.get("/", getAllBooks);
-router.post("/", addBook);
+router.get('/', getAllBooks);
+router.post('/', addBook);
 router.put("/:id", updateBook);
-router.delete("/:id", deleteBook);
+router.delete('/:id', deleteBook);
 
 export default router;
